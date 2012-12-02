033c5d7ba0b31907eea778ca3c5e129807432aa4
Ivan Zuñiga Perez <ivankruel@hotmail.com>
2012-12-02 16:59:57 -0600

    Update application/views/head/titulo.php

--- application/views/head/titulo.php
+++ application/views/head/titulo.php
@@ -1,6 +1,6 @@
 		<!-- Header with logo and headline -->
 		<header>
-			<?php echo anchor('welcome', ''. array('title' => 'Home'));?>
+			<?php echo anchor('welcome', '', array('title' => 'Home'));?>
 <!--
 			<h1><?php //echo MENSAJE_BIENVENIDA;?></h1>
 -->		</header>
